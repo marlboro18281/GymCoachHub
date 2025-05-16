5be17837029e4422f5e2a4a1f7090783a5b98c47
Anton <marlboro18281@gmail.com>
2025-05-16 13:08:08 +0300

    Add (Docker) initial configuration files for Apache and MySQL setup with database schema

--- src/components/config.php
+++ src/components/config.php
@@ -8,8 +8,9 @@ error_reporting(E_ALL);
 session_start();
 
 // Підключення до бази даних
-$mysqli = new mysqli("localhost", "root", "", "db_sport");
-
+//$mysqli = new mysqli("localhost", "root", "", "db_sport");
+//Підключення до бази docker
+$mysqli = new mysqli("mysql", "root", "rootpassword", "db_sport");
 if ($mysqli->connect_errno) {
     header("Content-Type: application/json; charset=utf-8");
     die(json_encode(["status" => "error", "message" => "Помилка підключення до БД: " . $mysqli->connect_error], JSON_UNESCAPED_UNICODE));
